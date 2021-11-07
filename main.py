4c92fde806ded487180bd7b15d01ff670c09816c
Dunque <roi.santos.rios@gmail.com>
2021-11-07 19:23:17 +0100

    morfologico sigue sin funcionar

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import re
 import numpy as np
 from natsort import natsorted, ns
 from skimage.util import img_as_uint
 from skimage.color import rgb2gray
 from skimage import data, exposure, img_as_float, io, color
 import matplotlib.pyplot as plt
+from skimage.filters import threshold_otsu
+import math
 
 imgPath = "img/"
 imgPathOut = "out/"
@@ -70,13 +73,12 @@ def filterImage(inImage, kernel):
 
 
 def gaussKernel1D (sigma):
-
     N = int(2*np.ceil(3*sigma)+ 1)
 
     result = np.zeros(N)
 
     mid = int(N/2)
-    result = [(1/(np.sqrt(2*np.pi)*sigma))*(1/(np.exp((i**2)/(2*sigma**2)))) for i in range(-mid,mid+1)]  
+    result = np.array([(1/(np.sqrt(2*np.pi)*sigma))*(1/(np.exp((i**2)/(2*sigma**2)))) for i in range(-mid,mid+1)])  
 
     return result/sum(result)
 
@@ -84,8 +86,6 @@ def gaussKernel1D (sigma):
 def gaussianFilter (inImage, sigma):
 
     gausskernel = gaussKernel1D(sigma)
-
-    print(gausskernel)
 
     image = filterImage(inImage, gausskernel)
 
@@ -95,13 +95,16 @@ def gaussianFilter (inImage, sigma):
 
 
 def medianFilter (inImage, filterSize):
+
     indexer = filterSize // 2
+
     window = [
         (i, j)
         for i in range(-indexer, filterSize-indexer)
         for j in range(-indexer, filterSize-indexer)
     ]
     index = len(window) // 2
+
     for i in range(len(inImage)):
         for j in range(len(inImage[0])):
             inImage[i][j] = sorted(
@@ -112,45 +115,104 @@ def medianFilter (inImage, filterSize):
                 ) else inImage[i+a][j+b]
                 for a, b in window
             )[index]
+
     return inImage  
 
 
-#---------------------------------------------------------------------------
+def highBoost (inImage, A, method, param):
 
-def dnorm(x, mu, sd):
-    return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
+    if method == "gaussian":
+        smooth = gaussianFilter(inImage, param)
+    elif method == "median":
+        smooth = medianFilter(inImage, param)
+    else:
+        raise Exception("Invalid method, it should be gaussian or median")
 
+    if A>=0:
+        outImage = A * inImage - smooth
+    else:
+        outImage = (A-1) * inImage + inImage - smooth
 
-def gaussian_kernel(size, sigma=1, verbose=False):
-    kernel_1D = np.linspace(-(size // 2), size // 2, size)
-    for i in range(size):
-        kernel_1D[i] = dnorm(kernel_1D[i], 0, sigma)
-    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
- 
-    kernel_2D *= 1.0 / kernel_2D.max()
- 
-    if verbose:
-        plt.imshow(kernel_2D, interpolation='none', cmap='gray')
-        plt.title("Kernel ( {}X{} )".format(size, size))
-        plt.show()
- 
-    return kernel_2D
- 
- 
-def gaussian_blur(inImage, kernel_size, verbose=False):
-    kernel = gaussian_kernel(kernel_size, sigma=np.sqrt(kernel_size), verbose=verbose)
-    return filterImage(inImage, kernel)
+    return outImage
 
 # 3.3 -----------------------------------------------------------------
+
+def erode(im,se):
+    rows,columns = im.shape[0], im.shape[1]
+    #Initialize counters (Just to keep track)
+    fit = 0
+    hit = 0
+    miss = 0
+
+    #Create a copy of the image to modified it´s pixel values
+    ero = np.copy(im)
+    #Specify kernel size (w*w)
+    w = 3
+
+    #
+    for i in range(rows-w-1):
+        for j in range(columns-w-1):
+            #Get a region (crop) of the image equal to kernel size
+            crop = im[i:w+i,j:w+j]
+            #Convert region of image to an array
+            img = np.array(crop)
+
+            #Get center
+            a = math.floor(w/2)
+            b = math.floor(w/2)
+            
+            #Initialize counters 
+            matches = 0
+            blacks = 0
+
+            #Count number of black pixels (0) and value matches between the two matrix
+            for x in range(w):
+                for y in range(w):
+                    #Count number of black pixels (0)
+                    if(img[x][y] == 0):
+                        blacks = blacks+1
+                        #Count number of matching pixel values between the two matrix   
+                        if (img[x][y] == se[x][y]):         
+                            matches = matches+1
+
+            #Test if structuring element fit crop image pixels
+            #If fit it does nothing because center pixel is already black
+            if(matches > 0):
+                if(matches == blacks):
+                    #Touch
+                    fit = fit + 1
+                    pass
+                #Test if structuring element hit crop image pixels
+                #If hit change ero center pixel to black
+                elif(matches < blacks):
+                    #Hit
+                    hit = hit+1
+                    ##PROBABLE ERROR IN HERE##
+                    ero[i+a][j+b] = 0
+            #If no pixel match just pass
+            else:
+                #Miss
+                miss=miss+1
+                pass
+
+    #Print the number of fits, hits, and misses
+    print(str(fit) + '\n' + str(hit) + '\n' + str(miss))
+
+    return ero
+
 # 3.4 -----------------------------------------------------------------
 
 def main():
 
-    #Import files
+    #Greyscale image
     filename = os.path.join(imgPath, 'bfly.jpeg')
     bfly = io.imread(filename, as_gray=True)
-    # original = data.astronaut()
-    # bfly = rgb2gray(original)
+
+    #Black and white image
+    filename = os.path.join(imgPath, 'bin.png')
+    bw = io.imread(filename, as_gray=True)
+    thresh = threshold_otsu(bw)
+    bw = bw > thresh
 
     #bfly = adjustIntensity(bfly, [10,100], [0,1])
 
@@ -164,12 +226,22 @@ def main():
 
     #bfly = gaussianFilter(bfly,1)
 
-    bfly = medianFilter(bfly,9)
+    #bfly = medianFilter(bfly,5)
+
+    #bfly = highBoost(bfly, 1, "gaussian", 1)
+    #bfly = highBoost(bfly, -1, "median", 1)
+
+    se = [[0,1,0],
+          [1,1,1],
+          [0,1,0]]
+
+    erode(bw,se)
 
     bfly = bfly / bfly.max()
-    io.imshow(bfly) 
-    io.show()
+    # io.imshow(bfly2, vmin=0, vmax=255) 
+    # io.show()
     io.imsave(os.path.join(imgPathOut, 'bflyOut.jpg'), bfly)
+    io.imsave(os.path.join(imgPathOut, 'bwOut.jpg'), bw)
 
 
 if __name__ =='__main__':
